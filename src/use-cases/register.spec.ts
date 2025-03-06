732a9cbb1453eff66dbd97d0d5ae8d743ed76176
Lucas Azevedo <lucas.shirabayashi@gmail.com>
2025-03-06 13:14:30 +0000

    feat: add fake in memory database to cover users test without touching the real database

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,7 +1,66 @@
 // teste da rota register
 
-import { expect, test } from 'vitest'
+import { describe, it, expect } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './error/user-already-exists-error'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user).toHaveProperty('id')
+    expect(user).toHaveProperty('name', 'John Doe')
+    expect(user).toHaveProperty('email', 'johndoe@example.com')
+  })
+
+  it('should hash user password upon registration', async () => {
+    // nao instanciamos o prismaUserRepository, pois nao queremos que ele bata no banco de dados
+    // por se tratar de um teste unitario, queremos que ele seja isolado do resto da app
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    // chamando de novo para dar o erro (email duplicado)
+    await expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError) // espero o erro que eu criei
+  })
 })
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -3,6 +3,7 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './error/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -10,10 +11,18 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -22,10 +31,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
